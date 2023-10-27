13bbdde51f7bde1ad740287a55d9c11389549ed5
Tanya <tanya.103.singh>
2023-10-27 22:51:51 +0530

    added widgets to the recommend page

--- wellnest/lib/views/recommend.dart
+++ wellnest/lib/views/recommend.dart
@@ -15,6 +15,8 @@ class _RecommendScreenState extends State<RecommendScreen> {
     int tag = widget.emotionTag;
     Color emotionColor = widget.emotionColor;
     List statement = ["We're sorry to hear that, Would you like to try any of the following?", "We're happy to know that! Would you like to try any of the following?"];
+    List activityNav = [['/notes/', '/options/', '/relax/'], ['/notes/', '/affirm/']];
+    List activities = [["Journal", "Meditate", "Relax"], ["Journal", "Affirm"]];
     return Scaffold(
       backgroundColor: emotionColor,
       appBar: AppBar(
@@ -42,24 +44,48 @@ class _RecommendScreenState extends State<RecommendScreen> {
                   SizedBox(
                     width: 300.0,
                     height: 150.0,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: emotionColor,
-                        elevation: 2.0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0)),
-                      ),
                       child: Center(
                         child: Padding(
-                          padding: const EdgeInsets.all(55.0),
+                          padding: const EdgeInsets.all(5.0),
                           child: Column(
                             children: [
                               Text(statement[tag],
                                   style: const TextStyle(
                                       color: Color.fromARGB(255, 60, 60, 60),
                                       fontWeight: FontWeight.bold,
-                                      fontSize: 22.0))
+                                      fontSize: 25.0))
+                            ],
+                          ),
+                        ),
+                      ),
+                  ),
+                  SizedBox(
+                    width: 150.0,
+                    height: 150.0,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pushNamed('/notes/');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromRGBO(221, 187, 204, 1),
+                        elevation: 2.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0)),
+                      ),
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Image.asset("assets/journal.png",
+                                  width: 80.0, height: 80.0),
+                              const SizedBox(height: 8.0),
+                              const Text("Journal",
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 60, 60, 60),
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18.0))
                             ],
                           ),
                         ),
@@ -67,61 +93,71 @@ class _RecommendScreenState extends State<RecommendScreen> {
                     ),
                   ),
                   SizedBox(
-                    width: 300.0,
+                    width: 150.0,
                     height: 150.0,
                     child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pushNamed('/affirm/');
+                      },
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: emotionColor,
+                        backgroundColor: const Color.fromRGBO(250, 243, 240, 1),
                         elevation: 2.0,
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(12.0)),
                       ),
                       child: Center(
                         child: Padding(
-                          padding: const EdgeInsets.all(55.0),
+                          padding: const EdgeInsets.all(8.0),
                           child: Column(
                             children: [
-                              Text("b",
-                                  style: const TextStyle(
+                              Image.asset("assets/affirm.png",
+                                  width: 80.0, height: 80.0),
+                              const SizedBox(height: 8.0),
+                              const Text("Affirm",
+                                  style: TextStyle(
                                       color: Color.fromARGB(255, 60, 60, 60),
                                       fontWeight: FontWeight.bold,
-                                      fontSize: 22.0))
+                                      fontSize: 18.0))
                             ],
                           ),
                         ),
                       ),
                     ),
                   ),
-                  
                   SizedBox(
-                    width: 300.0,
+                    width: 150.0,
                     height: 150.0,
                     child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pushNamed('/journal/');
+                      },
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: emotionColor,
+                        backgroundColor: const Color.fromRGBO(221, 187, 204, 1),
                         elevation: 2.0,
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(12.0)),
                       ),
                       child: Center(
                         child: Padding(
-                          padding: const EdgeInsets.all(55.0),
+                          padding: const EdgeInsets.all(8.0),
                           child: Column(
                             children: [
-                              Text("c",
-                                  style: const TextStyle(
+                              Image.asset("assets/journal.png",
+                                  width: 80.0, height: 80.0),
+                              const SizedBox(height: 8.0),
+                              const Text("Journal",
+                                  style: TextStyle(
                                       color: Color.fromARGB(255, 60, 60, 60),
                                       fontWeight: FontWeight.bold,
-                                      fontSize: 22.0))
+                                      fontSize: 18.0))
                             ],
                           ),
                         ),
                       ),
                     ),
                   ),
-                  
                 ],
               ),
             ),
